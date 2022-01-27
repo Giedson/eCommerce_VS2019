fbb0a66eabb7654e7eb06c6651f0f94e88fcbd1d
Giedson <giedson@msn.com>
2022-01-27 20:29:42 -0300

    Utilizando o Dapper.Contrib para construir um CRUD Simples de apenas uma tabela.

--- eCommerce.API/eCommerce.API/Models/Usuario.cs
+++ eCommerce.API/eCommerce.API/Models/Usuario.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using Dapper.Contrib.Extensions; // Adicionado para poder usar o Dapper.Contrib
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace eCommerce.API.Models
 {
+    [Table("Usuarios")] // Usando um anotation para o Dapper.Contrib Indicando o nome da Tabela a ser utilizada.
     public class Usuario
     {
+        [Key] // Usando um anotation para o Dapper.Contrib Indicando a Chave Primária
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }
@@ -17,16 +20,23 @@ namespace eCommerce.API.Models
         public string SituacaoCadastro { get; set; }
         public DateTimeOffset DataCadastro { get; set; } // Recomendado quando se tratar de um banco de dados pois utiliza o fuso horário.
 
+
         //One-To-One - Representa uma Composição. Indica o relacionamento(um pra um) do usuário com o contato, isto é, um usuário possui um contato.
+        // Usando um anotation para o Dapper.Contrib Indicando Contato deve ser Desconsiderado.
+        [Write(false)] 
         public Contato Contato { get; set; }
 
         //One-To-Many - Um usuário tem vários endereços podendo ser representado por uma coleção de endereços.Indica o relacionamento(um pra muitos)
+        
+        [Write(false)] 
         public ICollection<EnderecoEntrega> EnderecosEntrega { get; set; }
 
         //Many-To-Many - Um usuário tem vários departamentos e um departamento tem vários usuários.
         //Este tipo de relacionamento foi possível sem a criação da classe UsuariosDepartamentos porque
         //a tabela UsuariosDepartamentos possui apenas a pk, chave fk do usuario e chave fk do departamento
         //se houve qualquer outro tipo de campo teria se ser criada uma nova classe com o nome UsuariosDepartamentos.
+        // Usando um anotation para o Dapper.Contrib Indicando que o Departamento deve ser Desconsiderado.
+        [Write(false)]
         public ICollection<Departamento> Departamentos { get; set; }
     }
 }
---
+++ eCommerce.API/eCommerce.API/Repositories/ContribUsuarioRepository.cs
@@ -0,0 +1,47 @@
+﻿using eCommerce.API.Interfaces;
+using eCommerce.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Data; // Adicionado para poder usar o IDbConnection
+using System.Data.SqlClient; // Adicionado para poder usar o SqlConnection
+using Dapper.Contrib.Extensions; //  Adicionado para poder extender as opções de IDbConnection.
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.API.Repositories
+{
+    public class ContribUsuarioRepository : IUsuarioRepository
+    {
+        private IDbConnection _connection;
+        public ContribUsuarioRepository()
+        {
+            _connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=eCommerce;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        public List<Usuario> Get()
+        {
+            return _connection.GetAll<Usuario>().ToList();
+        }
+
+        public Usuario Get(int id)
+        {
+            return _connection.Get<Usuario>(id);
+        }
+
+        public void Insert(Usuario usuario)
+        {
+            // Tendo em vista que o Insert já retorna um long com o Identity da inserção não é necessário fazer um select para sobrecarregar.
+            usuario.Id = Convert.ToInt32(_connection.Insert<Usuario>(usuario)); 
+        }
+
+        public void Update(Usuario usuario)
+        {
+            _connection.Update<Usuario>(usuario);
+        }
+        public void Delete(int id)
+        {
+            var usuario = Get(id); // Chamando o Get da Classe que retorna o objeto Ususario.
+            _connection.Delete<Usuario>(usuario);
+        }
+    }
+}
